588fc2ecf6eb8c3bf66a4e3104f76b4b67a7fc48
fox <664088852@qq.com>
2023-07-10 22:32:52 +0800

    添加单例模式类封装

--- foxzt/log.h
+++ foxzt/log.h
@@ -5,4 +5,15 @@
 #ifndef CPPPROJ_LOG_H
 #define CPPPROJ_LOG_H
 
+#include <string>
+#include <memory>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <cstdint>
+#include <cstdarg>
+#include <list>
+#include "singleton.h"
+
 #endif //CPPPROJ_LOG_H
---
+++ foxzt/singleton.h
@@ -0,0 +1,35 @@
+//
+// Created by fox on 2023/7/10.
+// 单例模式封装
+
+#ifndef CPPPROJ_SINGLETON_H
+#define CPPPROJ_SINGLETON_H
+
+namespace foxzt {
+    template<class T, class X = void, int N = 0>
+    class Singleton {
+    public:
+        /**
+         * @brief 返回单例裸指针
+         */
+        static T *GetInstance() {
+            static T v;
+            return &v;
+        }
+    };
+
+    template<class T, class X = void, int N = 0>
+    class SingletonPtr {
+    public:
+        /**
+         * @brief 返回单例智能指针
+         */
+        static std::shared_ptr<T> GetInstance() {
+            static std::shared_ptr<T> v(new T);
+            return v;
+        }
+    };
+
+} // foxzt
+
+#endif //CPPPROJ_SINGLETON_H
