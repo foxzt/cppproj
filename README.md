3c9aff91dc83ef27419c74d44baf92d3abe74d69
fox <664088852@qq.com>
2023-07-14 10:39:34 +0800

    update README.md

--- README.md
+++ README.md
@@ -7,4 +7,39 @@ LogAppender
 LogFormatter
 Logger
 ```
-日志模块目前初步完成，再完成一些细节后，准备加入配置文件的支持，开发完后准备第二个模块：配置模块……
+日志模块基本完成，并包装了一些默认的宏方便使用，支持fmt库的格式化方式
+```cpp
+//使用示例
+#include<foxzt/foxzt.h>
+
+int main(){
+    FOXZT_INFO("hello {}","world !");
+    
+    return 0;
+}
+//[2023-07-14 10:32:40.043] [INFO] [/path/to/test.cpp:40]: hello world !
+```
+## 2、配置模块
+```cpp
+//通过这种方式生成一个配置信息
+foxzt::ConfigVar<int>::ptr g_int = foxzt::Config::Lookup<int>("system.port", 8080, "system port");
+```
+后续还会支持从yaml文件中读取相应的配置，例如：
+```yaml
+#log.yml
+logs:
+  - name: root
+    level: info
+    appenders:
+      - type: FileLogAppender
+        file: ./root.txt
+      - type: StdoutLogAppender
+  - name: system
+    level: info
+    appenders:
+      - type: FileLogAppender
+        file: ./system.txt
+      - type: StdoutLogAppender
+```
+程序在启动的时候会加载yml文件中的配置，生成两个日志器。
+功能模块正在开发中……
--- tests/test_environment/test_yamlcpp.cpp
+++ tests/test_environment/test_yamlcpp.cpp
@@ -1,73 +1,42 @@
 //
 // Created by fox on 2023/7/10.
 //
-#include <iostream>
 #include <yaml-cpp/yaml.h>
-#include <fstream>
+#include "foxzt/log.h"
 
-void fun() {
-    // 创建一个新的 YAML 节点
-    YAML::Node newConfig;
-    newConfig["name"] = "Alice";
-    newConfig["age"] = 25;
-    newConfig["hobbies"].push_back("Reading");
-    newConfig["hobbies"].push_back("Painting");
+void printYamlNode(const YAML::Node &node, int level = 0) {
+    // 打印节点的键和值
+    if (!node.IsNull()) {
+        for (int i = 0; i < level; ++i) {
+            std::cout << "  ";  // 缩进
+        }
 
-    // 将新的 YAML 节点写入文件
-    YAML::Emitter emitter;
-    emitter << newConfig;
-
-    std::ofstream fout("new_config.yaml");
-    fout << emitter.c_str();
-    fout.close();
-
-    std::cout << "New config file has been created." << std::endl;
+        if (node.IsScalar()) {
+            std::cout << "- " << node.Scalar() << std::endl;
+        } else if (node.IsSequence()) {
+            std::cout << "- [Sequence]" << std::endl;
+            for (const auto &element: node) {
+                printYamlNode(element, level + 1);
+            }
+        } else if (node.IsMap()) {
+            std::cout << "- {Map}" << std::endl;
+            for (const auto &pair: node) {
+                const auto &key = pair.first;
+                const auto &value = pair.second;
+                for (int i = 0; i < level + 1; ++i) {
+                    std::cout << "  ";  // 缩进
+                }
+                std::cout << key << ":";
+                printYamlNode(value, level + 1);
+            }
+        }
+    }
 }
 
-
 int main() {
-    // YAML 字符串
-    std::string yamlString = R"(
-        name: John
-        age: 30
-        hobbies:
-          - Reading
-          - Painting
-    )";
 
-    // 从字符串加载 YAML 数据
-    YAML::Node config = YAML::Load(yamlString);
-
-    // 从 YAML 节点中提取值
-    auto name = config["name"].as<std::string>();
-    int age = config["age"].as<int>();
-    auto hobbies = config["hobbies"].as<std::vector<std::string>>();
-
-    // 输出读取到的值
-    std::cout << "Name: " << name << std::endl;
-    std::cout << "Age: " << age << std::endl;
-    std::cout << "Hobbies: ";
-    for (const auto &hobby: hobbies) {
-        std::cout << hobby << " ";
-    }
-    std::cout << std::endl;
-
-    // 创建一个新的 YAML 节点
-    YAML::Emitter emitter;
-    emitter << YAML::BeginMap;
-    emitter << YAML::Key << "name" << YAML::Value << "Alice";
-    emitter << YAML::Key << "age" << YAML::Value << 25;
-    emitter << YAML::Key << "hobbies" << YAML::Value << YAML::BeginSeq << "Reading" << "Painting" << YAML::EndSeq;
-    emitter << YAML::EndMap;
-
-    // 获取生成的 YAML 字符串
-    std::string newConfigString = emitter.c_str();
-
-    // 输出新的 YAML 字符串
-    std::cout << "New Config:\n" << newConfigString << std::endl;
-
-
-    fun();
-
+    YAML::Node root = YAML::LoadFile("log.yml");
+    printYamlNode(root);
+    FOXZT_INFO("hello {}","world !");
     return 0;
 }
