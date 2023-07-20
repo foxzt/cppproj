d930a0ca930e452c59534e9373090695e371e39a
fox <664088852@qq.com>
2023-07-20 15:47:55 +0800

    更新项目结构

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -11,14 +11,15 @@ void test_log_config() {
     FOXZT_INFO("after: hello");
     FOXZT_LOGGER_ERROR(LOGGER_NAME_RAW("system"), "after: hello");
     std::set<int> vec{1, 2, 3, 2};
-    FOXZT_INFO("after: hello{}", vec);
+    FOXZT_INFO(fg(fmt::color::crimson) | fmt::emphasis::bold,
+               "Hello, {}!\n", "world");
 }
 
 
 int main() {
     test_log_config();
 
-    auto out = fmt::output_file("guide.txt");
+    auto out = fmt::output_file("guide.txt",std::ios_base::app);
     out.print("Don't {}", "Panic");
 
     return 0;
